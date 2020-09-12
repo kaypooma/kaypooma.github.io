957bf6e63e9aa592e815065cc3a601dd4bf457d3
kaypooma <kaypooma@gmail.com>
2020-09-12 08:44:22 -0700

    random color for

--- for/script.js
+++ for/script.js
@@ -1,33 +1,26 @@
-var c = ['CD4A4A', 'FF6E4A', 'FFCF48', 'BAB86C', '1CA9C9', '5D76CB', '8F509D'];
-
-function shuffle(array) {
-  var m = array.length, t, i;
-  while (m > 0) {
-    i = Math.floor(Math.random() * m--);
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  }
-
-  return array;
-}
+var c = [];
 
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-for (i = 0; i < 32; i++) {getRandomInt(0, c.length - 1)
+
+for (i=0; i<9; i++) {
+  c.push( `hsl(${getRandomInt(0,360)}, ${getRandomInt(40,60)}%, ${getRandomInt(40,60)}%)` )
+}
+
+for (i = 0; i < 32; i++) {
   let colorindex = getRandomInt(0, c.length - 1)
-  $('.sq-con').append(`<div class="sq" data-cindex="${colorindex}" style="background: #${c[colorindex]}"></div>`)
+  $('.sq-con').append(`<div class="sq" data-cindex="${colorindex}" style="background: ${c[colorindex]}"></div>`)
 }
 
 $('.sq').on('click', function(e) {
   var oC = $(this).css('background');
   let oInd = $(this).attr('data-cindex')
-  let nInd = (parseInt(oInd) + getRandomInt(1,6)) % 7
+  let nInd = (parseInt(oInd) + getRandomInt(1,6)) % c.length
 
-  $(this).css('background', `#${c[nInd]}`);
+  $(this).css('background', c[nInd]);
   $(this).attr('data-cindex', nInd);
 
   var eX = e.clientX;
