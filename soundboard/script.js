4f7cdef6336fb9c035d5d3e4a294eda0f2559248
kaypooma <kaypooma@gmail.com>
2022-02-10 19:25:33 -0800

    sound effects

--- soundboard/script.js
+++ soundboard/script.js
@@ -1,7 +1,7 @@
 // const sounds = ['21', 'airhorn', 'amogus_scream', 'amogus', 'aou', 'bonk', 'bruh', 'crowd_laughing', 'deez_nuts', 'dialup', 'ding', 'dubstep', 'eas_one', 'eas_two', 'epic_fail', 'fl_studio_render', 'flight_cry', 'flight_scream', 'fnaf_ambience', 'fnaf_jumpscare', 'geometry_dash', 'glass', 'grandma', 'jack_black', 'mammot', 'mouse_click', 'no_i_not', 'oh_my_god', 'porch', 'ps2_startup', 'pussy', 'ragdoll_sounds', 'reverb_fart', 'role_reveal', 'samsung_notification', 'squeak', 'suspense_1', 'suspense_2', 'suspense_3', 'suspense_4', 'suspense_5', 'suspense_6', 'swoosh', 'vine_boom', 'wet_fart', 'what_da_dog_doin', 'whip', 'wii_freeze', 'windows_95', 'windows_xp', 'wrong', 'you_stupid'];
 const sounds = [
     // speech
-    'you_stupid', 'no_i_not', '21', 'amogus_scream', 'amogus', 'aou', 'boyfriend', 'bruh', 'deez_nuts', 'flight_cry', 'flight_scream', 'grandma', 'jack_black', 'mammot', 'oh_my_god', 'porch', 'pussy', 'scream2', 'what_da_dog_doin', 'whip',
+    'you_stupid', 'no_i_not', '21', 'amogus_scream', 'amogus', 'aou', 'boyfriend', 'bruh', 'deez_nuts', 'flight_cry', 'flight_scream', 'grandma', 'jack_black', 'mammot', 'oh_my_god', 'porch', 'pussy', 'scream2', 'timallen', 'what_da_dog_doin', 'whip',
     '----',
     // misc
     'airhorn', 'bfdi_cheering', 'crowd_laughing', 'dialup', 'dubstep', 'eas_one', 'eas_two', 'epic_fail', 'fl_studio_render', 'fnaf_ambience', 'fnaf_jumpscare', 'geometry_dash', 'krabs', 'ps2_startup', 'ragdoll_sounds', 'role_reveal', 'samsung_notification', 'taco_bell', 'wii_freeze', 'windows_95', 'windows_xp',
@@ -10,10 +10,16 @@ const sounds = [
     'reverb_fart', 'wet_fart', 'short_fart', 
     '----',
     // short sounds
-    'bonk', 'boowomp', 'squeak', 'swoosh', 'ding', 'wrong', 'mouse_click', 'glass', 
+    'bonk', 'boowomp', 'squeak', 'swoosh', 'ding', 'wrong', 'mouse_click', 'glass', ']',
     '----',
     // suspense
-    'vine_boom', 'suspense_1', 'suspense_2', 'suspense_3', 'suspense_4', 'suspense_5', 'suspense_6'
+    'vine_boom', 'vine_boom_hd', 'suspense_1', 'suspense_2', 'suspense_3', 'suspense_4', 'suspense_5', 'suspense_6',
+    // sexer
+    '----',
+    'sexerass', 'sexerbigmouth', 'sexerbitch', 'sexerbitch2', 'sexerboner', 'sexerboom', 'sexerfuckyou', 'sexerstfu',
+    // taiko
+    '----',
+    'don', 'ka',
 ];
 const audioContext = new window.AudioContext();
 
